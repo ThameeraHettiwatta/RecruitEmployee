0be16d94b69686ee470135ceaea2b0dad351bce6
thameera-lh <thameeralakshan.16@cse.mrt.ac.lk>
2021-03-25 17:15:24 +0530

    modified the folder structure as a classic monolithic application

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import {Component, OnInit} from '@angular/core';
+import {Employee} from './employee';
+import {EmployeeService} from './employee.service';
+import {HttpErrorResponse} from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  public employees: Employee[];
+
+  constructor(private employeeService: EmployeeService){}
+
+  ngOnInit() {
+    this.getEmployees();
+  }
+
+  public getEmployees(): void {
+    this.employeeService.getEmployees().subscribe(
+      (response: Employee[]) => {
+        this.employees = response;
+        console.log(this.employees);
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+}
