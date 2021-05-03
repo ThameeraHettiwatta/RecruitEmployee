6b2cb6c32038e732bd2a8e35ecad1d96b973b7b5
thameera-lh <thameeralakshan.16@cse.mrt.ac.lk>
2021-05-03 15:01:21 +0530

    resolved the issue of api based url in frontend angular app

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   // apiBaseUrl: "http://localhost:8080"
-  apiBaseUrl: "http://192.168.49.2:31498"
+  apiBaseUrl: "http://spring-boot-svc:31498"
 };
 
 /*
